9112dc409829f513ca2b2c1f37f684d0d53ffeca
Daniel Park <dpeace926@gmail.com>
2019-06-19 19:10:25 -0400

    query launch and rocket data, display

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,9 @@
 import React from "react";
 import ApolloClient from "apollo-boost";
 import { ApolloProvider } from "react-apollo";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 import Launches from "./components/Launches";
+import Launch from "./components/Launch";
 import "./App.css";
 
 const client = new ApolloClient({
@@ -11,14 +13,17 @@ const client = new ApolloClient({
 function App() {
   return (
     <ApolloProvider client={client}>
-      <div className="container">
-        <img
-          src="https://hdwallsource.com/img/2017/3/spacex-logo-wallpaper-59810-61599-hd-wallpapers.jpg"
-          alt="SpaceX logo"
-          style={{ width: 300, display: "block", margin: "auto" }}
-        />
-        <Launches />
-      </div>
+      <Router>
+        <div className="container">
+          <img
+            src="https://hdwallsource.com/img/2017/3/spacex-logo-wallpaper-59810-61599-hd-wallpapers.jpg"
+            alt="SpaceX logo"
+            style={{ width: 300, display: "block", margin: "auto" }}
+          />
+          <Route exact path="/" component={Launches} />
+          <Route exact path="/launch/:flight_number" component={Launch} />
+        </div>
+      </Router>
     </ApolloProvider>
   );
 }
